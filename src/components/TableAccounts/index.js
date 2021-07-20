2a05f1fc4134620fa626b3337c091edbd40bdf65
jocsakesley <jocsadm@gmail.com>
2021-07-20 18:32:15 -0300

    Formatando total na tabela

--- src/components/TableAccounts/index.js
+++ src/components/TableAccounts/index.js
@@ -1,63 +1,48 @@
-import { TableContainer, Table, TableHead, TableRow, TableCell, TableBody, makeStyles } from "@material-ui/core"
+import { TableContainer, Table, TableHead, TableRow, TableCell, TableBody } from "@material-ui/core"
 import React from "react";
 import { InputContext, OutputContext } from "../../providers/accounts"
+import { useStyles } from "./styles";
 
-
-const useStyles = makeStyles(theme => ({
-    table: {
-        marginTop: theme.spacing(10),
-        borderColor: theme.palette.primary
-    },
-    tableHead: {
-        borderBottom:"5px solid #1194a8", 
-        color:"#1194a8"
-    }
-
-}))
 
 export const TableAccounts = () => {
     const { input } = React.useContext(InputContext)
     const { output } = React.useContext(OutputContext)
     const classes = useStyles()
-
+    let inp = 0
+    let out = 0
     return (
-        <>
         <TableContainer>
-            <Table className={classes.table}>
-                <TableHead className={classes.tableHead}>
-                    <TableRow >
-                        <TableCell style={{color:"#1194a8"}}>Entradas</TableCell>
-                        <TableCell style={{color:"#1194a8"}}>Data</TableCell>
-                        <TableCell style={{color:"#1194a8"}}>Valor</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
+         <Table className={classes.table}>
+             <TableHead className={classes.tableHeadInput}>
+                 <TableRow >
+                     <TableCell style={{color:"#1194a8"}}>Entradas</TableCell>
+                     <TableCell style={{color:"#1194a8"}}>Data</TableCell>
+                     <TableCell style={{color:"#1194a8"}}>Valor</TableCell>
+                 </TableRow>
+             </TableHead>
+             <TableBody>
 
-                    {input.map(i => {
-                        return (
-                        <TableRow>
-                        
-                            <TableCell style={{color:"#1194a8"}}>{i.nameAccount}</TableCell>
-                            <TableCell style={{color:"#1194a8"}}>{i.date}</TableCell>
-                            <TableCell style={{color:"#1194a8"}}>{i.value}</TableCell>
-                       
-                        </TableRow>
-                    )})}
-                    <TableRow>
-                        <TableCell colSpan={2}>Total</TableCell>
-                        <TableCell >{input.reduce((accum, v) => {
-                        
-                        return accum += parseInt(v.value)
-                        
-                    }, 0)}</TableCell>
-                    </TableRow>
+                 {input.map(i => {
+                     return (
+                     <TableRow>
+                     
+                         <TableCell style={{color:"#1194a8"}}>{i.nameAccount}</TableCell>
+                         <TableCell style={{color:"#1194a8"}}>{i.date}</TableCell>
+                         <TableCell style={{color:"#1194a8"}}>{i.value}</TableCell>
                     
-                </TableBody>
-            </Table>
-        </TableContainer>
-         <TableContainer>
-         <Table margin="normal">
-             <TableHead style={{borderBottom:"5px solid #F0584A", color:"#F0584A"}}>
+                     </TableRow>
+                 )})}
+                 <TableRow>
+                    <TableCell rowSpan={3} />
+                     <TableCell rowSpan={2}>Total</TableCell>
+                     <TableCell >{inp = input.reduce((accum, input) => {
+                         return accum += parseInt(input.value)
+                     }, 0)}</TableCell>
+                 </TableRow>
+                 
+                 
+             </TableBody>
+             <TableHead className={classes.tableHeadOutput}>
                  <TableRow >
                      <TableCell style={{color:"#F0584A"}}>Saídas</TableCell>
                      <TableCell style={{color:"#F0584A"}}>Data</TableCell>
@@ -77,17 +62,24 @@ export const TableAccounts = () => {
                      </TableRow>
                  )})}
                  <TableRow>
-                     <TableCell colSpan={2}>Total</TableCell>
-                     <TableCell >{output.reduce((accum, output) => {
-                        
+                    <TableCell rowSpan={3} />
+                     <TableCell rowSpan={2}>Total</TableCell>
+                     <TableCell >{out = output.reduce((accum, output) => {
                          return accum += parseInt(output.value)
                      }, 0)}</TableCell>
                  </TableRow>
                  
+                 
              </TableBody>
-         </Table>
+             <TableHead>
+                 <TableRow>
+                 <TableCell rowSpan={2} />
+                     <TableCell colSpan={1}>Total Geral</TableCell>
+                  
+                     <TableCell >{ inp - out }</TableCell>
+                 </TableRow>
+             </TableHead>
+         </Table>       
      </TableContainer>
-     </>
-
     )
 }
---
+++ src/components/TableAccounts/styles.js
@@ -0,0 +1,18 @@
+import { makeStyles } from "@material-ui/core";
+
+
+export const useStyles = makeStyles(theme => ({
+    table: {
+        marginTop: theme.spacing(10),
+        borderColor: theme.palette.primary
+    },
+    tableHeadInput: {
+        borderBottom:"5px solid #1194a8", 
+        color:"#1194a8"
+    },
+    tableHeadOutput: {
+        borderBottom:"5px solid #F0584A", 
+        color:"#F0584A"
+    }
+
+}))
